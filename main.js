54431afacebf50a3f195597449d0e9df14f9bf10
Leonardo Marqueti de Lima <94808998+leomarqueti@users.noreply.github.com>
2022-03-20 10:51:31 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,470 @@
+var carta1 = {
+    nome: 'Serius',
+    codigo: '001',
+    tipo: 'Maga',
+    imagem: 'card2-a.png',
+    bg: '#8cc4be',
+    atributos: {
+      ataque: 390,
+      defesa: 200,
+      velocidade: 140
+    }
+  }
+  
+  var carta2 = {
+    nome: 'Corki',
+    codigo: '001',
+    tipo: 'Orc',
+    imagem: 'card1-a.png',
+    bg: '#99a068',
+    atributos: {
+      ataque:280,
+      defesa: 300,
+      velocidade: 130
+    }
+  }
+  
+  var carta3 = {
+    nome: 'Wolfener',
+    codigo: '003',
+    tipo: 'Lobo',
+    imagem: 'card3-a.png',
+    bg: '#a37e6b',
+    atributos: {
+      ataque: 170,
+      defesa: 320,
+      velocidade: 300
+    }
+  }
+  
+  var carta4 = {
+    nome: 'Leviart',
+    codigo: '004',
+    tipo: 'Guerreiro',
+    imagem: 'card4-a.png',
+    bg: '#efdda7',
+    atributos: {
+      ataque: 320,
+      defesa: 300,
+      velocidade: 150
+    }
+  }
+  
+  var carta5 = {
+    nome: 'Azagner',
+    codigo: '005',
+    tipo: 'Anão',
+    imagem: 'card5-a.png',
+    bg: '#565656',
+    atributos: {
+      ataque: 200,
+      defesa: 320,
+      velocidade: 100
+    }
+  }
+  
+  var carta6 = {
+    nome: 'Dragarius',
+    codigo: '006',
+    tipo: 'Dragão',
+    imagem: 'card6-a.png',
+    bg: '#c3776d',
+    atributos: {
+      ataque: 300,
+      defesa: 150,
+      velocidade: 350
+    }
+  }
+
+  var carta7 = {
+    nome: 'Tourinus',
+    codigo: '007',
+    tipo: 'Touro',
+    imagem: 'card7-a.png',
+    bg: '#ffc687',
+    atributos: {
+      ataque: 200,
+      defesa: 290,
+      velocidade: 230
+    }
+  }
+
+  var carta8 = {
+    nome: 'Donandurbar',
+    codigo: '008',
+    tipo: 'Mago',
+    imagem: 'card8-a.png',
+    bg: '#72a7e2',
+    atributos: {
+      ataque: 320,
+      defesa: 200,
+      velocidade: 100
+    }
+  }
+
+  var carta9 = {
+    nome: 'Aguinaitor',
+    codigo: '009',
+    tipo: 'Aguia',
+    imagem: 'card9-a.png',
+    bg: '#e3ac45',
+    atributos: {
+      ataque: 200,
+      defesa: 210,
+      velocidade: 340
+    }
+  }
+
+  var carta10 = {
+    nome: 'Morto de Monkaicity',
+    codigo: '010',
+    tipo: 'Zombie',
+    imagem: 'card10-a.png',
+    bg: '#abefe9',
+    atributos: {
+      ataque: 200,
+      defesa: 220,
+      velocidade: 20
+    }
+  }
+
+  var carta11 = {
+    nome: 'Monique Mashine',
+    codigo: '011',
+    tipo: 'Guerreira',
+    imagem: 'card11-a.png',
+    bg: '#e7d39c',
+    atributos: {
+      ataque: 300,
+      defesa: 300,
+      velocidade: 180
+    }
+  }
+
+  var carta12 = {
+    nome: 'Laiter Minimus',
+    codigo: '012',
+    tipo: 'Lobo',
+    imagem: 'card12-a.png',
+    bg: '#e7d39c',
+    atributos: {
+      ataque: 290,
+      defesa: 150,
+      velocidade: 270
+    }
+  }
+
+  var carta13 = {
+    nome: 'Senhor Arnold Jusi',
+    codigo: '013',
+    tipo: 'Anão',
+    imagem: 'card13-a.png',
+    bg: '#32342e',
+    atributos: {
+      ataque: 200,
+      defesa: 250,
+      velocidade: 100
+    }
+  }
+
+  var carta14 = {
+    nome: 'Crinika',
+    codigo: '013',
+    tipo: 'Meio-Humana',
+    imagem: 'card14-a.png',
+    bg: '#eebf8d',
+    atributos: {
+      ataque: 190,
+      defesa: 250,
+      velocidade: 200
+    }
+  }
+
+  var carta15 = {
+    nome: 'Dragoner TRUNFO',
+    codigo: '015',
+    tipo: 'Deuses',
+    imagem: 'card15-a.png',
+    bg: '#ff658e',
+    atributos: {
+      ataque: 800,
+      defesa: 700,
+      velocidade: 700
+    }
+  }
+
+  var carta16 = {
+    nome: 'Vanefe TRUNFO',
+    codigo: '016',
+    tipo: 'Deuses',
+    imagem: 'card16-a.png',
+    bg: '#ffab4d',
+    atributos: {
+      ataque: 700,
+      defesa: 800,
+      velocidade: 600
+    }
+  }
+
+  var carta17 = {
+    nome: 'Rasputinos',
+    codigo: '017',
+    tipo: 'Ratos',
+    imagem: 'card17-a.png',
+    bg: '#dfb79e',
+    atributos: {
+      ataque: 120,
+      defesa: 50,
+      velocidade: 70
+    }
+  }
+
+  var carta18 = {
+    nome: 'Rapernius',
+    codigo: '018',
+    tipo: 'Ratos',
+    imagem: 'card18-a.png',
+    bg: '#dfb79e',
+    atributos: {
+      ataque: 50,
+      defesa: 100,
+      velocidade: 20
+    }
+  }
+
+  var carta19 = {
+    nome: 'Ranika',
+    codigo: '019',
+    tipo: 'Ratos',
+    imagem: 'card19-a.png',
+    bg: '#dfb79e',
+    atributos: {
+      ataque: 50,
+      defesa: 50,
+      velocidade: 190
+    }
+  }
+
+  var carta20 = {
+    nome: 'Georgino de Riniva',
+    codigo: '020',
+    tipo: 'Bruxo',
+    imagem: 'card20-a.png',
+    bg: '#c1b28f',
+    atributos: {
+      ataque: 320,
+      defesa: 215,
+      velocidade: 160
+    }
+  }
+
+  var carta21 = {
+    nome: 'Purilus',
+    codigo: '021',
+    tipo: 'Passaro',
+    imagem: 'card21-a.png',
+    bg: '#e3b385',
+    atributos: {
+      ataque: 290,
+      defesa: 190,
+      velocidade: 280
+    }
+  }
+
+  var carta22 = {
+    nome: 'Wolner',
+    codigo: '022',
+    tipo: 'Lobo',
+    imagem: 'card22-a.png',
+    bg: '#7b746f',
+    atributos: {
+      ataque: 280,
+      defesa: 140,
+      velocidade: 280
+    }
+  }
+
+  var carta23 = {
+    nome: 'Serno',
+    codigo: '023',
+    tipo: 'Sereia',
+    imagem: 'card23-a.png',
+    bg: '#88b6b0',
+    atributos: {
+      ataque: 310,
+      defesa: 190,
+      velocidade: 200
+    }
+  }
+
+  var cartas = [carta1, carta2, carta3, carta4, carta5, carta6, carta7, carta8, carta9, carta10, carta11, carta12, carta13, carta14, carta15, carta16, carta17, carta18, carta19, carta20, carta21, carta22, carta23]
+  var cartaMaquina
+  var cartaJogador
+
+  var pontosJogador = 0;
+  var pontosMaquina = 0;
+
+  
+  placar();
+
+  function sortearCarta() {
+    var numeroCartaJogador = parseInt(Math.random() * 23)
+    cartaJogador = cartas[numeroCartaJogador]
+  
+    var numeroCartaMaquina = parseInt(Math.random() * 23)
+    cartaMaquina = cartas[numeroCartaMaquina]
+  
+    document.getElementById('btnSortear').disabled = true
+    document.getElementById('btnJogar').disabled = false
+  
+    //   exibirOpcoes()
+    exibirCartaJogador()
+    audio.play()
+  }
+  
+  // function exibirOpcoes() {
+  //   var opcoes = document.getElementById('opcoes')
+  //   var opcoesTexto = ''
+  
+  //   for (var atributo in cartaJogador.atributos) {
+  //     opcoesTexto += `<input type='radio' name='atributo' value='${atributo}'> <strong>Susto:</strong>${atributo}`
+  //   }
+  // }
+  
+  function obtemAtributoSelecionado() {
+    var radioAtributos = document.getElementsByName('atributo')
+  
+    for (var i = 0; i < radioAtributos.length; i++) {
+      if (radioAtributos[i].checked == true) {
+        return radioAtributos[i].value
+      }
+    }
+  }
+  
+  function jogar() {
+    var atributoSelecionado = obtemAtributoSelecionado()
+    var divResultado = document.getElementById('resultado')
+
+    
+  
+    if (
+      cartaJogador.atributos[atributoSelecionado] >
+      cartaMaquina.atributos[atributoSelecionado]
+    ) {
+      htmlResultado = `<p class="resultado">Isso aí humano! Você conseguiu!</p>`;
+      pontosJogador++
+      proximaRodada()
+      
+    } else if (
+      cartaJogador.atributos[atributoSelecionado] <
+      cartaMaquina.atributos[atributoSelecionado]
+    ) {
+      htmlResultado = `<p class="resultado">Perdeu!
+      Cuidado!<br> O bicho pega e não solta mais!</p>`
+      pontosMaquina++
+      proximaRodada()
+      
+    } else {
+      htmlResultado = `<p class='resultado'>Empatou! Você e eu somos um time!</p>`
+      proximaRodada()
+    }
+  
+    divResultado.innerHTML = htmlResultado
+  
+    document.getElementById('btnJogar').disabled = true
+  
+    placar()
+    exibirCartaMaquina()
+    
+    if (pontosJogador > 7){
+      aparacer();
+      pontosJogador = 0
+      pontosMaquina = 0
+    } else if (pontosMaquina > 7) {
+      aparacerQuandoPerder()
+      pontosJogador = 0
+      pontosMaquina = 0
+    } else {
+      proximaRodada(); 
+    }
+    
+  }
+  
+  function exibirCartaJogador() {
+    var divCartaJogador = document.getElementById('carta-jogador')
+  
+    var opcoesTexto = ``
+    for (var atributo in cartaJogador.atributos) {
+      opcoesTexto += `<li class='radio'><input type='radio' name='atributo' value='${atributo}'> <strong>${atributo}:</strong> ${cartaJogador.atributos[atributo]}</li>`
+    }
+  
+    divCartaJogador.innerHTML = `<div class="card-top" style="background-color: ${cartaJogador.bg}">
+      <div class="details">
+        <h2 class="name">${cartaJogador.nome}</h2>
+        <span>${cartaJogador.codigo}</span>
+      </div>
+      <span class="type">${cartaJogador.tipo}</span>
+      <div class="image-card">
+        <img src="${cartaJogador.imagem}"/>
+      </div>
+    </div>
+    <div class="card-info">
+      <div class="status">
+        <ul id="opcoes">
+          ${opcoesTexto}
+        </ul>
+      </div>
+    </div>`
+  }
+  
+  function exibirCartaMaquina() {
+    var divCartaMaquina = document.getElementById('carta-maquina')
+  
+    var opcoesTexto = ``
+    for (var atributo in cartaMaquina.atributos) {
+      opcoesTexto += `<li name='atributo' value='${atributo}'> <strong>${atributo}:</strong> ${cartaMaquina.atributos[atributo]}</li>`
+    }
+  
+    divCartaMaquina.innerHTML = `<div class="card-top" style="background-color: ${cartaMaquina.bg}">
+        <div class="details">
+          <h2 class="name">${cartaMaquina.nome}</h2>
+          <span>${cartaMaquina.codigo}</span>
+        </div>
+        <span class="type">${cartaMaquina.tipo}</span>
+        <div class="image-card">
+          <img src="${cartaMaquina.imagem}"/>
+        </div>
+      </div>
+      <div class="card-info">
+        <div class="status">
+          <ul id="opcoes">
+            ${opcoesTexto}
+          </ul>
+        </div>
+      </div>`
+  }
+
+  function proximaRodada(){
+    document.getElementById('btnSortear').disabled = false
+  }
+
+  
+
+  function placar() {
+    let divPlacar = document.getElementById('placar');
+    let html = "Jogador " + pontosJogador + "/" + pontosMaquina + " Maquina";
+
+    divPlacar.innerHTML = html
+  }
+
+  function aparacer(){
+    let divFinal = document.getElementById("final").style.display = "block";
+    
+    divFinal.innerHTML = divFinal
+  }
+
+  function aparacerQuandoPerder(){
+    let divFinal = document.getElementById("final-perder").style.display = "block";
+    
+    divFinal.innerHTML = divFinal
+  }
